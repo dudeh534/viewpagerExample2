6f16a2a385540247841da7c4d1d6855357dbaab7
Youngdo <dudeh534@naver.com>
2015-12-31 19:07:25 +0900

    ViewPagerTest

---
+++ app/src/main/java/com/ourincheon/viewpagertest/MainActivity.java
@@ -0,0 +1,65 @@
+package com.ourincheon.viewpagertest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+
+	private ViewPager viewPager;
+	private ArrayList<View> pageViews; 	// pages view
+	private ImageView imageView;        // image => small white and black point
+	private ImageView[] imageViews;
+	private ViewGroup viewPics;         // layout for view group
+	private ViewGroup viewPoints;       // layout for small points
+	
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // save views into array
+        LayoutInflater inflater = getLayoutInflater();
+        pageViews = new ArrayList<View>();
+        pageViews.add(inflater.inflate(R.layout.page01, null));
+        pageViews.add(inflater.inflate(R.layout.page02, null));
+        pageViews.add(inflater.inflate(R.layout.page03, null));
+        
+        // save small points into array
+        imageViews = new ImageView[pageViews.size()];
+        viewPics = (ViewGroup) inflater.inflate(R.layout.view_pics, null);
+        
+        // find view by id
+        viewPoints = (ViewGroup) viewPics.findViewById(R.id.viewGroup);
+        viewPager = (ViewPager) viewPics.findViewById(R.id.guidePages);
+        
+        // set images
+        for(int i=0;i<pageViews.size();i++){
+        	imageView = new ImageView(MainActivity.this);
+        	imageView.setLayoutParams(new LayoutParams(50,50));
+        	imageView.setPadding(20, 0, 20, 0);
+        	imageViews[i] = imageView;
+        	
+        	if(i==0){
+        		imageViews[i].setBackgroundResource(R.drawable.circle_white);
+        	}else{
+        		imageViews[i].setBackgroundResource(R.drawable.circle_grey);
+        	}
+        	
+        	viewPoints.addView(imageViews[i]);
+        }
+        setContentView(viewPics);
+        
+        // set page adapter and listener
+        viewPager.setAdapter(new GuidePageAdapter(pageViews, this));
+        viewPager.setOnPageChangeListener(new GuidePageChangeListener(imageViews));
+
+    }
+}
